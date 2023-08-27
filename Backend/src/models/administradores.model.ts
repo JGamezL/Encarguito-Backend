7de3151e0bf3c93fbfedb8a7cf9b4a27080eae37
Jose David Gamez Lara <102843337+JGamezL@users.noreply.github.com>
2023-08-27 01:12:56 -0600

    Delete Backend directory

--- Backend/src/models/administradores.model.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-export interface administradores {
-    id: String;
-    nombre: String;
-    apellido: String;
-    correo: String;
-    contrasena: String;
-}
--- Backend/src/models/empresas.model.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-export interface empresas {
-    id: Number;
-    nombreEmpresa: String;
-    nombreContacto: String;
-    telefono: String;
-    direccion: String;
-}
